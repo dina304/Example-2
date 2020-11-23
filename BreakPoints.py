39c4ecaf6fe3b33f94d61acda7312b54457f76ef
Dina <dina.goldberg@controlup.com>
2020-11-23 19:09:13 +0200

    Initial commit

---
+++ BreakPoints.py
@@ -0,0 +1,11 @@
+
+def loop_it_out(num_of_rounds):
+    sum=0
+    if num_of_rounds%2!=0:
+        for i in range(1,num_of_rounds):
+            sum=sum+i
+    return sum
+
+
+result=loop_it_out(5)
+print(result)
